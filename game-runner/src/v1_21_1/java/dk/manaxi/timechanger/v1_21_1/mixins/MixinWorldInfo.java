6da7a811c334139d073084e0e3363c33f8a4c818
Itz_Hoffe <mhoff770@gmail.com>
2024-08-27 19:28:07 +0200

    Add support to 1.21.1

---
+++ game-runner/src/v1_21_1/java/dk/manaxi/timechanger/v1_21_1/mixins/MixinWorldInfo.java
@@ -0,0 +1,24 @@
+package dk.manaxi.timechanger.v1_21_1.mixins;
+
+import dk.manaxi.timechanger.core.TimeChanger;
+import net.minecraft.client.multiplayer.ClientLevel.ClientLevelData;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Shadow;
+
+@Mixin(ClientLevelData.class)
+public class MixinWorldInfo {
+  @Shadow
+  private long dayTime;
+  /**
+   * @author Itz_Hoffe
+   * @reason Set the time to the custom time if enabled
+   */
+  @Overwrite
+  public long getDayTime() {
+    if(TimeChanger.instance.configuration().customTime().get() && TimeChanger.instance.configuration().enabled().get()) {
+      return TimeChanger.instance.getTime();
+    }
+    return dayTime;
+  }
+}
