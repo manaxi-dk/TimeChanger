210ae164a8970e9cfa3b39fdd70fc0f50415c9fc
Itz_Hoffe <mhoff770@gmail.com>
2024-12-05 00:27:33 +0100

    Add support to 1.21.4

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,7 @@
 rootProject.name = "timechanger"
 
 pluginManagement {
-    val labyGradlePluginVersion = "0.5.6"
+    val labyGradlePluginVersion = "0.5.7"
     buildscript {
         repositories {
             maven("https://dist.labymod.net/api/v1/maven/release/")
