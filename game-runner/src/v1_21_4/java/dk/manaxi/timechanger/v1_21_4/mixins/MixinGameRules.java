3fd65f393d44a3f9b8384d67a20f0c5209d95509
XXL Steve <80220423+CornetPanique86@users.noreply.github.com>
2025-01-11 23:07:36 +0100

    Replaced Inject with Mixin

--- game-runner/src/v1_21_4/java/dk/manaxi/timechanger/v1_21_4/mixins/MixinGameRules.java
+++ game-runner/src/v1_21_4/java/dk/manaxi/timechanger/v1_21_4/mixins/MixinGameRules.java
@@ -4,17 +4,26 @@ import dk.manaxi.timechanger.core.TimeChanger;
 import net.minecraft.world.level.GameRules;
 import net.minecraft.world.level.GameRules.BooleanValue;
 import net.minecraft.world.level.GameRules.Key;
+import net.minecraft.world.level.GameRules.Value;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Shadow;
 
 @Mixin(GameRules.class)
-public class MixinGameRules {
-  @Inject(method = "getBoolean", at = @At("HEAD"), cancellable = true)
-  private void forceDaylightCycle(Key<BooleanValue> lvt_1_1_, CallbackInfoReturnable<Boolean> cir) {
-    if (TimeChanger.instance.configuration().forceDaylightCycle().get() && lvt_1_1_.equals(GameRules.RULE_DAYLIGHT)) {
-      cir.setReturnValue(true);
+public abstract class MixinGameRules {
+  @Shadow
+  public abstract <T extends Value<T>> T getRule(Key<T> $$0);
+
+  /**
+   * @author XXL_Steve
+   * @reason Always make daylight cycle game rule return true if option is enabled
+   */
+  @Overwrite
+  public boolean getBoolean(Key<BooleanValue> gameRule) {
+    System.out.println("Oh, so it really does check the game rule...");
+    if (gameRule.equals(GameRules.RULE_DAYLIGHT) && TimeChanger.instance.configuration().forceDaylightCycle().get() && TimeChanger.instance.configuration().enabled().get()) {
+      return true;
     }
+    return ((BooleanValue)this.getRule(gameRule)).get();
   }
 }
--- game-runner/src/v1_21_4/java/dk/manaxi/timechanger/v1_21_4/mixins/MixinWorldInfo.java
+++ game-runner/src/v1_21_4/java/dk/manaxi/timechanger/v1_21_4/mixins/MixinWorldInfo.java
@@ -3,16 +3,23 @@ package dk.manaxi.timechanger.v1_21_4.mixins;
 import dk.manaxi.timechanger.core.TimeChanger;
 import net.minecraft.client.multiplayer.ClientLevel.ClientLevelData;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Shadow;
 
 @Mixin(ClientLevelData.class)
 public class MixinWorldInfo {
-  @Inject(method = "getDayTime", at = @At("HEAD"), cancellable = true)
-  private void returnCustomDayTime(CallbackInfoReturnable<Long> cir) {
+  @Shadow
+  private long dayTime;
+  /**
+   * @author Itz_Hoffe
+   * @reason Set the time to the custom time if enabled
+   */
+  @Overwrite
+  public long getDayTime() {
+    System.out.println("get day time    adzadazdzdzaddqsds");
     if(!TimeChanger.instance.configuration().forceDaylightCycle().get() && TimeChanger.instance.configuration().enabled().get()) {
-      cir.setReturnValue(TimeChanger.instance.getTime());
+      return TimeChanger.instance.getTime();
     }
+    return dayTime;
   }
 }
