e4e5859406d6b07447ebebd696be0c48d67e6856
Itz_Hoffe <mhoff770@gmail.com>
2025-08-20 14:22:09 +0200

    Add mixins and support for Minecraft 1.21.8
    Introduced MixinClientLevel and MixinClientLevelData for version 1.21.8 to handle custom time and daylight cycle logic. Updated gradle.properties to include 1.21.8 in the supported Minecraft versions.

---
+++ game-runner/src/v1_21_8/java/dk/manaxi/timechanger/v1_21_8/mixins/MixinClientLevelData.java
@@ -0,0 +1,29 @@
+package dk.manaxi.timechanger.v1_21_8.mixins;
+
+import dk.manaxi.timechanger.core.TimeChanger;
+import net.minecraft.client.multiplayer.ClientLevel.ClientLevelData;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(ClientLevelData.class)
+public class MixinClientLevelData {
+  @Shadow
+  private long dayTime;
+
+  @Inject(method = "setDayTime", at = @At("HEAD"), cancellable = true)
+  private void setCustomDayTime(long $$0, CallbackInfo ci) {
+    if (TimeChanger.instance.configuration().enabled().get()) {
+      if(TimeChanger.instance.configuration().forceDaylightCycle().get()) {
+        dayTime = TimeChanger.instance.getAndIncrementDayTime();
+        ci.cancel();
+      } else {
+        dayTime = TimeChanger.instance.getTime();
+        ci.cancel();
+      }
+    }
+  }
+
+}
